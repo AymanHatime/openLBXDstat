4362c7ab89f4280720318d163fbbd9ec5898906d
AymanHatime <aymanhatimedev@gmail.com>
2026-04-03 23:31:23 +0200

    Unzip user datas

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const userData = document.getElementById('lbzip');
 const userDataStatus = document.getElementById('lbzip-status');
+const processDataButton = document.getElementById('process-data-btn');
+
+let file = null;
 
 userData.addEventListener('change', () => {
-    const file = userData.files[0];
+    processDataButton.disabled = true;
+    file = userData.files[0];
     
     if (!file) {
         userDataStatus.textContent = 'No file selected.';
@@ -10,8 +14,26 @@ userData.addEventListener('change', () => {
         const fileName = file.name.toLowerCase();
         if (fileName.endsWith('.zip')) {
             userDataStatus.textContent = `Selected file: ${file.name}`;
+            processDataButton.disabled = false;
         } else {
-            userDataStatus.textContent = 'Please select a valid .zip file.';
+            userDataStatus.textContent = `You selected ${file.name} Please select a valid .zip file.`;
+            file = null;
         }
+    }
+});
+
+processDataButton.addEventListener('click', async () => {
+    const sectionProcessingStatus = document.getElementById('upload-section-processing-status');
+    userData.disabled = true;
+    processDataButton.disabled = true;
+    sectionProcessingStatus.textContent = 'Processing...';
+
+    try {
+        const zip = await JSZip.loadAsync(file);    
+        console.log(zip);
+            
+    } catch (error) {
+        sectionProcessingStatus.textContent = `Error processing file: ${error.message}`;
+        userData.disabled = false;
     }
 });
