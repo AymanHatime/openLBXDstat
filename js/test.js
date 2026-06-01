b8d97bf6be981aa876be1ea85291d6faaac66aac
AymanHatime <aymanhatimedev@gmail.com>
2026-06-01 02:26:07 +0200

    dashboard total movies stylized

---
+++ js/test.js
@@ -0,0 +1,857 @@
+[
+    [
+        "Date",
+        "Name",
+        "Year",
+        "Letterboxd URI"
+    ],
+    [
+        "2025-03-13",
+        "Fly Me to the Moon",
+        "2024",
+        "https://boxd.it/A7Xg"
+    ],
+    [
+        "2025-03-13",
+        "Mickey 17",
+        "2025",
+        "https://boxd.it/q1Dk"
+    ],
+    [
+        "2025-03-13",
+        "Flow",
+        "2024",
+        "https://boxd.it/v1Ei"
+    ],
+    [
+        "2025-03-13",
+        "Heretic",
+        "2024",
+        "https://boxd.it/H1lY"
+    ],
+    [
+        "2025-03-13",
+        "Companion",
+        "2025",
+        "https://boxd.it/F04o"
+    ],
+    [
+        "2025-03-13",
+        "The Monkey",
+        "2025",
+        "https://boxd.it/Gwiu"
+    ],
+    [
+        "2025-03-13",
+        "Captain America: Brave New World",
+        "2025",
+        "https://boxd.it/uYDm"
+    ],
+    [
+        "2025-03-13",
+        "Nosferatu",
+        "2024",
+        "https://boxd.it/f5eG"
+    ],
+    [
+        "2025-03-13",
+        "Wicked",
+        "2024",
+        "https://boxd.it/e8ME"
+    ],
+    [
+        "2025-03-13",
+        "Interstellar",
+        "2014",
+        "https://boxd.it/4VZ8"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Prisoner of Azkaban",
+        "2004",
+        "https://boxd.it/29Qy"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Philosopher's Stone",
+        "2001",
+        "https://boxd.it/29QS"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Goblet of Fire",
+        "2005",
+        "https://boxd.it/29Qo"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Chamber of Secrets",
+        "2002",
+        "https://boxd.it/29QI"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Deathly Hallows: Part 2",
+        "2011",
+        "https://boxd.it/1RYk"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Order of the Phoenix",
+        "2007",
+        "https://boxd.it/29Qe"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Half-Blood Prince",
+        "2009",
+        "https://boxd.it/29FU"
+    ],
+    [
+        "2025-03-14",
+        "Harry Potter and the Deathly Hallows: Part 1",
+        "2010",
+        "https://boxd.it/1RYu"
+    ],
+    [
+        "2025-03-14",
+        "Hit Man",
+        "2023",
+        "https://boxd.it/ANB2"
+    ],
+    [
+        "2025-03-14",
+        "The Heir Apparent: Largo Winch",
+        "2008",
+        "https://boxd.it/1Obu"
+    ],
+    [
+        "2025-03-14",
+        "Largo Winch II",
+        "2011",
+        "https://boxd.it/mXo"
+    ],
+    [
+        "2025-03-14",
+        "The Price of Money: A Largo Winch Adventure",
+        "2024",
+        "https://boxd.it/BNNu"
+    ],
+    [
+        "2025-03-14",
+        "The Fall Guy",
+        "2024",
+        "https://boxd.it/s0Bm"
+    ],
+    [
+        "2025-03-14",
+        "Dragon Ball Z: Battle of Gods",
+        "2013",
+        "https://boxd.it/4a0C"
+    ],
+    [
+        "2025-03-14",
+        "Marked Men: Rule + Shaw",
+        "2025",
+        "https://boxd.it/ENOS"
+    ],
+    [
+        "2025-03-14",
+        "Se7en",
+        "1995",
+        "https://boxd.it/29zs"
+    ],
+    [
+        "2025-03-14",
+        "September 5",
+        "2024",
+        "https://boxd.it/JYsI"
+    ],
+    [
+        "2025-03-14",
+        "The Tuche Family",
+        "2011",
+        "https://boxd.it/1RE"
+    ],
+    [
+        "2025-03-14",
+        "God Save the Tuches",
+        "2025",
+        "https://boxd.it/H3Ac"
+    ],
+    [
+        "2025-03-14",
+        "The Tuche Family: The American Dream",
+        "2016",
+        "https://boxd.it/cOXg"
+    ],
+    [
+        "2025-03-14",
+        "The Magic Tuche",
+        "2018",
+        "https://boxd.it/f1Is"
+    ],
+    [
+        "2025-03-14",
+        "Christmas with the Tuches",
+        "2021",
+        "https://boxd.it/mFWI"
+    ],
+    [
+        "2025-03-14",
+        "Lost Highway",
+        "1997",
+        "https://boxd.it/29Wc"
+    ],
+    [
+        "2025-03-14",
+        "Dune",
+        "2021",
+        "https://boxd.it/fA7G"
+    ],
+    [
+        "2025-03-14",
+        "Dune: Part Two",
+        "2024",
+        "https://boxd.it/pUfA"
+    ],
+    [
+        "2025-03-14",
+        "The First Omen",
+        "2024",
+        "https://boxd.it/fx08"
+    ],
+    [
+        "2025-03-27",
+        "Novocaine",
+        "2025",
+        "https://boxd.it/JmdA"
+    ],
+    [
+        "2025-12-14",
+        "Bugonia",
+        "2025",
+        "https://boxd.it/qeWY"
+    ],
+    [
+        "2025-12-14",
+        "Five Nights at Freddy's 2",
+        "2025",
+        "https://boxd.it/KEvY"
+    ],
+    [
+        "2025-12-14",
+        "Zootopia 2",
+        "2025",
+        "https://boxd.it/F0vO"
+    ],
+    [
+        "2025-12-14",
+        "Wicked: For Good",
+        "2025",
+        "https://boxd.it/AyfK"
+    ],
+    [
+        "2025-12-14",
+        "F1",
+        "2025",
+        "https://boxd.it/yjVM"
+    ],
+    [
+        "2025-12-14",
+        "Rush",
+        "2013",
+        "https://boxd.it/3lyS"
+    ],
+    [
+        "2025-12-14",
+        "Ford v Ferrari",
+        "2019",
+        "https://boxd.it/ce74"
+    ],
+    [
+        "2025-12-17",
+        "Fight Club",
+        "1999",
+        "https://boxd.it/2a9q"
+    ],
+    [
+        "2025-12-17",
+        "Home Alone",
+        "1990",
+        "https://boxd.it/29Fg"
+    ],
+    [
+        "2025-12-17",
+        "Chainsaw Man – The Movie: Reze Arc",
+        "2025",
+        "https://boxd.it/Kgyu"
+    ],
+    [
+        "2025-12-17",
+        "The Running Man",
+        "2025",
+        "https://boxd.it/u36A"
+    ],
+    [
+        "2025-12-17",
+        "Avatar",
+        "2009",
+        "https://boxd.it/1Efm"
+    ],
+    [
+        "2025-12-17",
+        "Caught Stealing",
+        "2025",
+        "https://boxd.it/LnyS"
+    ],
+    [
+        "2025-12-17",
+        "Sinners",
+        "2025",
+        "https://boxd.it/KQMM"
+    ],
+    [
+        "2025-12-17",
+        "The Roses",
+        "2025",
+        "https://boxd.it/MjnI"
+    ],
+    [
+        "2025-12-17",
+        "Home Alone 2: Lost in New York",
+        "1992",
+        "https://boxd.it/29F6"
+    ],
+    [
+        "2025-12-17",
+        "Catch Me If You Can",
+        "2002",
+        "https://boxd.it/29VS"
+    ],
+    [
+        "2025-12-17",
+        "Avatar: The Way of Water",
+        "2022",
+        "https://boxd.it/2E2E"
+    ],
+    [
+        "2025-12-17",
+        "Superman",
+        "2025",
+        "https://boxd.it/E9IU"
+    ],
+    [
+        "2025-12-17",
+        "Materialists",
+        "2025",
+        "https://boxd.it/GZlW"
+    ],
+    [
+        "2025-12-17",
+        "Now You See Me: Now You Don't",
+        "2025",
+        "https://boxd.it/f3i2"
+    ],
+    [
+        "2025-12-17",
+        "Five Nights at Freddy's",
+        "2023",
+        "https://boxd.it/ikHu"
+    ],
+    [
+        "2025-12-17",
+        "Weapons",
+        "2025",
+        "https://boxd.it/EMTM"
+    ],
+    [
+        "2025-12-17",
+        "One Battle After Another",
+        "2025",
+        "https://boxd.it/DUHM"
+    ],
+    [
+        "2025-12-17",
+        "Train to Busan",
+        "2016",
+        "https://boxd.it/dSQU"
+    ],
+    [
+        "2025-12-17",
+        "Keeper",
+        "2025",
+        "https://boxd.it/N8lI"
+    ],
+    [
+        "2025-12-23",
+        "The Housemaid",
+        "2025",
+        "https://boxd.it/QFEO"
+    ],
+    [
+        "2025-12-26",
+        "Anaconda",
+        "2025",
+        "https://boxd.it/KV9g"
+    ],
+    [
+        "2025-12-26",
+        "Heidi: Rescue of the Lynx",
+        "2025",
+        "https://boxd.it/zEua"
+    ],
+    [
+        "2025-12-26",
+        "JUJUTSU KAISEN: Execution",
+        "2025",
+        "https://boxd.it/XjRA"
+    ],
+    [
+        "2025-12-26",
+        "The Baronesses",
+        "2025",
+        "https://boxd.it/LP9m"
+    ],
+    [
+        "2025-12-26",
+        "Roofman",
+        "2025",
+        "https://boxd.it/LcIW"
+    ],
+    [
+        "2025-12-26",
+        "Exit 8",
+        "2025",
+        "https://boxd.it/S9Kk"
+    ],
+    [
+        "2025-12-26",
+        "Kaamelott: The Second Chapter (Part I)",
+        "2025",
+        "https://boxd.it/EJCo"
+    ],
+    [
+        "2025-12-28",
+        "Splitsville",
+        "2025",
+        "https://boxd.it/PGtA"
+    ],
+    [
+        "2025-12-28",
+        "The Party's Over!",
+        "2025",
+        "https://boxd.it/O7qY"
+    ],
+    [
+        "2025-12-28",
+        "Drive",
+        "2011",
+        "https://boxd.it/IG"
+    ],
+    [
+        "2025-12-29",
+        "Old Dads",
+        "2023",
+        "https://boxd.it/Bk0o"
+    ],
+    [
+        "2026-01-02",
+        "Bullet Train",
+        "2022",
+        "https://boxd.it/qW26"
+    ],
+    [
+        "2026-01-02",
+        "Pawn Sacrifice",
+        "2014",
+        "https://boxd.it/7dlE"
+    ],
+    [
+        "2026-01-02",
+        "Gladiator",
+        "2000",
+        "https://boxd.it/2b9m"
+    ],
+    [
+        "2026-01-02",
+        "Wolf Man",
+        "2025",
+        "https://boxd.it/qzSI"
+    ],
+    [
+        "2026-01-03",
+        "Stranger Things 5: The Finale",
+        "2025",
+        "https://boxd.it/YqXW"
+    ],
+    [
+        "2026-01-08",
+        "The Hating Game",
+        "2021",
+        "https://boxd.it/mhiE"
+    ],
+    [
+        "2026-01-10",
+        "Freedom Writers",
+        "2007",
+        "https://boxd.it/28p2"
+    ],
+    [
+        "2026-01-12",
+        "La La Land",
+        "2016",
+        "https://boxd.it/a5fa"
+    ],
+    [
+        "2026-01-12",
+        "Eternity",
+        "2025",
+        "https://boxd.it/LYtK"
+    ],
+    [
+        "2026-01-17",
+        "What Happened to Monday",
+        "2017",
+        "https://boxd.it/ejUu"
+    ],
+    [
+        "2026-01-17",
+        "Greenland",
+        "2020",
+        "https://boxd.it/j1Um"
+    ],
+    [
+        "2026-01-17",
+        "Avatar: Fire and Ash",
+        "2025",
+        "https://boxd.it/2W7s"
+    ],
+    [
+        "2026-01-19",
+        "Capernaum",
+        "2018",
+        "https://boxd.it/iM3s"
+    ],
+    [
+        "2026-01-19",
+        "Sirāt",
+        "2025",
+        "https://boxd.it/HxJo"
+    ],
+    [
+        "2026-01-19",
+        "Sonic the Hedgehog",
+        "2020",
+        "https://boxd.it/gdK8"
+    ],
+    [
+        "2026-01-20",
+        "Greenland 2: Migration",
+        "2026",
+        "https://boxd.it/vH5O"
+    ],
+    [
+        "2026-01-20",
+        "Sans pitié",
+        "2025",
+        "https://boxd.it/WhyO"
+    ],
+    [
+        "2026-01-20",
+        "Sonic the Hedgehog 2",
+        "2022",
+        "https://boxd.it/pb8O"
+    ],
+    [
+        "2026-01-21",
+        "Sonic the Hedgehog 3",
+        "2024",
+        "https://boxd.it/zq0U"
+    ],
+    [
+        "2026-01-22",
+        "Mercy",
+        "2026",
+        "https://boxd.it/KYv0"
+    ],
+    [
+        "2026-01-22",
+        "Late Shift",
+        "2025",
+        "https://boxd.it/QgzO"
+    ],
+    [
+        "2026-01-22",
+        "Follow Me",
+        "2020",
+        "https://boxd.it/movq"
+    ],
+    [
+        "2026-01-25",
+        "Send Help",
+        "2026",
+        "https://boxd.it/JuhA"
+    ],
+    [
+        "2026-01-26",
+        "The Housemaid",
+        "2010",
+        "https://boxd.it/Q6k"
+    ],
+    [
+        "2026-01-30",
+        "The Wing or the Thigh?",
+        "1976",
+        "https://boxd.it/29GS"
+    ],
+    [
+        "2026-01-31",
+        "Love and Monsters",
+        "2020",
+        "https://boxd.it/lJ6E"
+    ],
+    [
+        "2026-01-31",
+        "American Assassin",
+        "2017",
+        "https://boxd.it/eFLE"
+    ],
+    [
+        "2026-02-01",
+        "Le Crocodile du Botswanga",
+        "2014",
+        "https://boxd.it/7hrw"
+    ],
+    [
+        "2026-02-05",
+        "Scream",
+        "1996",
+        "https://boxd.it/24EC"
+    ],
+    [
+        "2026-02-07",
+        "Scream 2",
+        "1997",
+        "https://boxd.it/24Es"
+    ],
+    [
+        "2026-02-09",
+        "The Restaurant",
+        "1966",
+        "https://boxd.it/1EWU"
+    ],
+    [
+        "2026-02-13",
+        "Scream 3",
+        "2000",
+        "https://boxd.it/24Ei"
+    ],
+    [
+        "2026-02-15",
+        "Fifty Shades of Grey",
+        "2015",
+        "https://boxd.it/6qLo"
+    ],
+    [
+        "2026-02-15",
+        "Fifty Shades Darker",
+        "2017",
+        "https://boxd.it/bm2E"
+    ],
+    [
+        "2026-02-15",
+        "Fifty Shades Freed",
+        "2018",
+        "https://boxd.it/bb5C"
+    ],
+    [
+        "2026-02-17",
+        "No Other Choice",
+        "2025",
+        "https://boxd.it/nKiA"
+    ],
+    [
+        "2026-02-17",
+        "Kaizen",
+        "2024",
+        "https://boxd.it/PRJY"
+    ],
+    [
+        "2026-02-17",
+        "It Ends with Us",
+        "2024",
+        "https://boxd.it/ENjg"
+    ],
+    [
+        "2026-02-17",
+        "Gladiator II",
+        "2024",
+        "https://boxd.it/kqQQ"
+    ],
+    [
+        "2026-02-17",
+        "The Godfather",
+        "1972",
+        "https://boxd.it/2aNK"
+    ],
+    [
+        "2026-02-17",
+        "The Godfather Part II",
+        "1974",
+        "https://boxd.it/2aNq"
+    ],
+    [
+        "2026-02-17",
+        "The Godfather Part III",
+        "1990",
+        "https://boxd.it/2aN6"
+    ],
+    [
+        "2026-02-19",
+        "Crime 101",
+        "2026",
+        "https://boxd.it/IoGs"
+    ],
+    [
+        "2026-02-19",
+        "Scream 4",
+        "2011",
+        "https://boxd.it/Zmc"
+    ],
+    [
+        "2026-02-22",
+        "Hamnet",
+        "2025",
+        "https://boxd.it/woVy"
+    ],
+    [
+        "2026-02-23",
+        "Marty Supreme",
+        "2025",
+        "https://boxd.it/Off0"
+    ],
+    [
+        "2026-02-23",
+        "Wonka",
+        "2023",
+        "https://boxd.it/tCQm"
+    ],
+    [
+        "2026-02-23",
+        "Dune: Part Three",
+        "2026",
+        "https://boxd.it/ImWe"
+    ],
+    [
+        "2026-02-23",
+        "Scream",
+        "2022",
+        "https://boxd.it/o0lg"
+    ],
+    [
+        "2026-02-24",
+        "Scream VI",
+        "2023",
+        "https://boxd.it/zdfW"
+    ],
+    [
+        "2026-02-24",
+        "Death of a Unicorn",
+        "2025",
+        "https://boxd.it/HCPm"
+    ],
+    [
+        "2026-02-24",
+        "Top Gun",
+        "1986",
+        "https://boxd.it/29JC"
+    ],
+    [
+        "2026-02-24",
+        "Mission: Impossible – The Final Reckoning",
+        "2025",
+        "https://boxd.it/l7A2"
+    ],
+    [
+        "2026-02-25",
+        "Back to the Future",
+        "1985",
+        "https://boxd.it/2b8e"
+    ],
+    [
+        "2026-02-26",
+        "Scream 7",
+        "2026",
+        "https://boxd.it/HRvQ"
+    ],
+    [
+        "2026-02-26",
+        "Top Gun: Maverick",
+        "2022",
+        "https://boxd.it/cjr4"
+    ],
+    [
+        "2026-02-28",
+        "Wuthering Heights",
+        "2026",
+        "https://boxd.it/OdIa"
+    ],
+    [
+        "2026-02-28",
+        "Child's Play",
+        "1988",
+        "https://boxd.it/1VTo"
+    ],
+    [
+        "2026-02-28",
+        "Child's Play 2",
+        "1990",
+        "https://boxd.it/1Uqq"
+    ],
+    [
+        "2026-03-15",
+        "The Bride!",
+        "2026",
+        "https://boxd.it/HSw6"
+    ],
+    [
+        "2026-03-15",
+        "Project Hail Mary",
+        "2026",
+        "https://boxd.it/pEeQ"
+    ],
+    [
+        "2026-03-16",
+        "Les K d'Or",
+        "2026",
+        "https://boxd.it/NqZw"
+    ],
+    [
+        "2026-03-18",
+        "Reminders of Him",
+        "2026",
+        "https://boxd.it/QEnW"
+    ],
+    [
+        "2026-03-22",
+        "Police Flash 80",
+        "2026",
+        "https://boxd.it/BSBe"
+    ],
+    [
+        "2026-03-23",
+        "Nuremberg",
+        "2025",
+        "https://boxd.it/K6qU"
+    ],
+    [
+        "2026-03-23",
+        "Parasite",
+        "2019",
+        "https://boxd.it/hTha"
+    ],
+    [
+        ""
+    ]
+]
